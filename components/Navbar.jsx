0c93e5c28c2935a7b03d0eacb09d06872dc0703b
AYUSH ARYA <ayush@brightdigitalgold.com>
2023-07-17 14:02:44 +0530

    header done

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -14,19 +14,15 @@ const Navbar = () => (
     className={`${styles.xPaddings} py-8 relative`}
   >
     <div className="absolute w-[50%] inset-0 gradient-01" />
-    <div className="{`${styles.innerWidth} mx-auto
-    flex justify-between gap-8}"
-    >
-      <img src="/search.svg"
-        alt="search"
-        className="w-[24px] h-[24px] object-contain"
-      />
-      <h2 className='="font-extrabold text-[24px] leading-[30px] text-white'>TechBotics</h2>
-      <img
-        src="/menu.svg"
-        alt="menu"
-        className="w-[24px] h-[24px] object-contain"
-      />
+    <div className={`${styles.innerWidth} mx-auto flex justify-between gap-8`}>
+      <h2 className='="font-extrabold text-[34px] leading-[30px] text-white font-black'>TechBotics</h2>
+      <div className={` mx-30px gap-8 flex `}>
+        <h2 className='="font-extrabold text-[24px] leading-[30px] text-white '>Tech_1</h2>
+        <h2 className='="font-extrabold text-[24px] leading-[30px] text-white '>Tech_2</h2>
+        <h2 className='="font-extrabold text-[24px] leading-[30px] text-white '>Tech_2</h2>
+      </div>
+
+
 
     </div>
 
